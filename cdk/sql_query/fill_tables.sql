74d110fda5d18d3b7f31268b73404d754037824c
Volha_Bakharava <olavlad777@gmail.com>
2024-08-01 09:10:36 +0300

    AWS-RSS-HW-8 Refactored

--- cdk/sql_query/fill_tables.sql
+++ cdk/sql_query/fill_tables.sql
@@ -1,7 +1,11 @@
 INSERT INTO carts (id, user_id, created_at, updated_at, status)
 VALUES
-('a6316a1c-55ce-4b51-b635-f12299889929', 'bbd2d1b0-5f27-4dd5-87ac-0f58e2f4a0fa', CURRENT_DATE, CURRENT_DATE, 'OPEN');
+('a6316a1c-55ce-4b51-b635-f12299889929', 'bbd2d1b0-5f27-4dd5-87ac-0f58e2f4a0fa', '2024-07-23', '2024-07-28', 'OPEN'),
+('d0270d65-7a2a-4749-a441-99f44cc70ee1', '94c16f43-41c3-4772-896a-c8f7fd169365', '2024-07-23', '2024-07-28', 'OPEN'),
+('caeeecaf-7fa5-4ff7-b1e5-ab85ba27fecf', '41fafb73-00da-4614-a562-93811a4ed67f', '2024-07-23', '2024-07-28', 'ORDERED');
 
 INSERT INTO cart_items (cart_id, product_id, count)
 VALUES
-('46bfee79-7fc3-4853-b191-b7698ba4da75', 'b194ab9f-e5f6-4ef7-8666-dd5c18b03974', 4);
+('a6316a1c-55ce-4b51-b635-f12299889929', 'b194ab9f-e5f6-4ef7-8666-dd5c18b03974', 4),
+('caeeecaf-7fa5-4ff7-b1e5-ab85ba27fecf', 'dbdbbafe-d7d5-451b-be99-05fbeb9e1086', 6),
+('d0270d65-7a2a-4749-a441-99f44cc70ee1', '88912f01-931e-41e1-8a12-e3d776288e89', 12);
